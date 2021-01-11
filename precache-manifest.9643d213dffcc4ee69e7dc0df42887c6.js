cd029111d28ed7db1bd07ff2a6ea07e5d7d6d2d0
Lukas Simianer <lsimianer@gmail.com>
2021-01-11 12:20:27 -0600

    Updates

---
+++ precache-manifest.9643d213dffcc4ee69e7dc0df42887c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37dd4dae7797b253cdce7c4ad6262ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9974c684e7dd8d9c65d5",
+    "url": "/static/css/main.32925779.chunk.css"
+  },
+  {
+    "revision": "8d16d681309b015a9b53",
+    "url": "/static/js/2.27f23fbb.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.27f23fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9974c684e7dd8d9c65d5",
+    "url": "/static/js/main.bb5e2abc.chunk.js"
+  },
+  {
+    "revision": "d776ec43f96e59403982",
+    "url": "/static/js/runtime-main.6accf6de.js"
+  },
+  {
+    "revision": "79686a1c88a2792ba9c510c49dc10dea",
+    "url": "/static/media/blackNavBG.79686a1c.jpg"
+  },
+  {
+    "revision": "cdb04d491989bff4fbb88534aa9d0d4a",
+    "url": "/static/media/hexLogo.cdb04d49.png"
+  },
+  {
+    "revision": "2698993019b7537abd007e9844181e2e",
+    "url": "/static/media/prettyPicture.26989930.jpg"
+  }
+]);
